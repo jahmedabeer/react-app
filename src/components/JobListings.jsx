fa1528a067a8a4240d9e4b62a940c0a246f77f66
Jak <jahmedabeer05@gmail.com>
2025-05-24 11:50:59 +0600

    fetch data from json-server

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect, use } from 'react';
 import JobListing from './JobListing'
-import jobs from '../jobs.json'
 
 const JobListings = ({ isHome = false }) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log('Error fetching jobs:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchJobs();
+  }, [])
 
   return (
     <section className="bg-blue-50 px-4 py-10">
@@ -11,10 +28,14 @@ const JobListings = ({ isHome = false }) => {
           Browse Jobs
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {
-            jobListings.map((job) => (
-              <JobListing key={job.id} job={job} />
-            ))
+          {loading ? (<h2>Loading...</h2>) :
+            <>
+              {
+                jobs.map((job) => (
+                  <JobListing key={job.id} job={job} />
+                ))
+              }
+            </>
           }
         </div>
       </div>
